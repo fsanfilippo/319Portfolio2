5ff5fa10b0bee3820510ffa4e5b3990e70469e29
Frank San Filippo <sanflip77@gmail.com>
2017-12-09 23:31:48 -0600

    made final changes

--- AngularPart/src/app/address/address.component.ts
+++ AngularPart/src/app/address/address.component.ts
@@ -16,6 +16,9 @@ export class AddressComponent implements OnInit {
   pollingLocations: string[];
   pollingLocationsTimes: string[];
   pollingLocationsNotes: string[];
+  confirmAddress: boolean = false;
+  normalizedAddress: string;
+  setAddress: boolean = false;
 
   
   constructor(private addressService: AddressService) { 
@@ -40,40 +43,30 @@ export class AddressComponent implements OnInit {
   }
 
   onSelect(election: Election): void {
-    this.selectedElection = election;
-    console.log(election.id);
+    if(!this.address){
+      this.setAddress = true;
+      return;
+    }
     
+    
+    console.log(election.id);
+
     this.addressService.getPolling(this.address, election.id).subscribe(data => {
      
       this.voterInfo = data;
       console.log(this.voterInfo);
-      console.log(this.voterInfo.election);
-
-      if(this.voterInfo['pollingLocations']){
-        this.pollingLocations = new Array<string>();
-        console.log(this.voterInfo.pollingLoactions);
-        for(let i = 0; i < this.voterInfo['pollingLocations'].length;i++){
-          this.pollingLocations.push("Polling Location Address: "+
-            this.voterInfo.pollingLocations[i+""].address.locationName+" "
-            +this.voterInfo.pollingLocations[i+""].address.line1+" "
-            +this.voterInfo.pollingLocations[i+""].address.city+" "
-            +this.voterInfo.pollingLocations[i+""].address.state+" "
-            +this.voterInfo.pollingLocations[i+""].address.zip
-          );
-          this.pollingLocations.push("Polling Location Hours: "+this.voterInfo.pollingLocations[i+""].pollingHours);
-          this.pollingLocations.push("Polling Location Notes: "+this.voterInfo.pollingLocations[i+""].pollingHours);
-        }
-      }else{
-        this.pollingLocations = new Array<string>();
-        console.log("No Polling Locations");
-        this.pollingLocations.push("No Polling Locations");
-      }
+      this.selectedElection = election;
     });
 
   }
 
-  onClick(){
-    console.log(this.address);
+  getCorrectAddress(){
+    this.addressService.getPolling(this.address, 2000).subscribe(data => {
+      this.confirmAddress = true;
+      let input = data.normalizedInput;
+      this.normalizedAddress = input.line1 + ' ' + input.city + ', ' + input.state + ' ' + input.zip;
+
+    });
 
     
   }
